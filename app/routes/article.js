f9bbda635db9cec764edb9e919a16e683e85dc32
Pat O'Callaghan <pat@intercom.io>
2019-12-04 23:38:06 +0000

    Migrate routes to native classes

--- app/routes/article.js
+++ app/routes/article.js
@@ -1,11 +1,11 @@
-import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import Route from '@ember/routing/route';
 
-export default Route.extend({
-  session: service(),
-  store: service(),
+export default class ArticleRoute extends Route {
+  @service session;
+  @service store;
 
   model({ slug }) {
     return this.store.findRecord('article', slug);
-  },
-});
+  }
+}
--- app/routes/editor/new.js
+++ app/routes/editor/new.js
@@ -1,12 +1,12 @@
-import Route from '@ember/routing/route';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import Route from '@ember/routing/route';
 import window from 'ember-window-mock';
 
-export default Route.extend({
-  session: service(),
-  store: service(),
-
-  templateName: 'editor/new',
+export default class NewRoute extends Route {
+  @service session;
+  @service store;
+  templateName = 'editor/new';
 
   /**
    * Only authenticated users are allowed to visit this page.
@@ -17,47 +17,46 @@ export default Route.extend({
     if (!this.session.isLoggedIn) {
       this.transitionTo('login');
     }
-  },
+  }
 
   /**
    * Create a new article record used in the page.
    */
   model() {
     return this.store.createRecord('article');
-  },
+  }
 
-  actions: {
+  /**
+   * If there are changes to the article, confirm with the user before transitioning.
+   * If user continues to transition routes, rollback the changes.
+   * Rollback changes will unload it from the store if the article is new.
+   */
+  @action
+  async willTransition(transition) {
+    const { draftArticle } = this.controller;
+    let shouldRollbackAttributes = false;
+
     /**
-     * If there are changes to the article, confirm with the user before transitioning.
-     * If user continues to transition routes, rollback the changes.
-     * Rollback changes will unload it from the store if the article is new.
+     * If there are dirty attributes, comfirm with user whether they want to leave withou saving.
+     * Otherwise, just roll back the model attributes, which will unload it from the store.
      */
-    async willTransition(transition) {
-      const { draftArticle } = this.controller;
-      let shouldRollbackAttributes = false;
-
-      /**
-       * If there are dirty attributes, comfirm with user whether they want to leave withou saving.
-       * Otherwise, just roll back the model attributes, which will unload it from the store.
-       */
-      if (draftArticle.hasDirtyAttributes) {
-        if (window.confirm('You have not created the article. Are you sure you want to leave?')) {
-          shouldRollbackAttributes = true;
-        } else {
-          transition.abort();
-        }
-      } else {
+    if (draftArticle.hasDirtyAttributes) {
+      if (window.confirm('You have not created the article. Are you sure you want to leave?')) {
         shouldRollbackAttributes = true;
+      } else {
+        transition.abort();
       }
+    } else {
+      shouldRollbackAttributes = true;
+    }
 
-      if (!transition.isAborted && shouldRollbackAttributes) {
-        /**
-         * Rollback attributes after transition to prevent the article content in the form to flash.
-         */
-        await transition;
+    if (!transition.isAborted && shouldRollbackAttributes) {
+      /**
+       * Rollback attributes after transition to prevent the article content in the form to flash.
+       */
+      await transition;
 
-        draftArticle.rollbackAttributes();
-      }
-    },
-  },
-});
+      draftArticle.rollbackAttributes();
+    }
+  }
+}
